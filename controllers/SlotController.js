8ff21700b9b7086c4a15f5a9d96957f6bdd4db11
zgjimzhubajj <zgjimzhubaj1@gmail.com>
2024-05-01 14:21:51 +0200

    KAN-7 created controllers for endpoints

---
+++ controllers/SlotController.js
@@ -0,0 +1,41 @@
+import Slot from "../models/slots.js";
+
+// Function used to create new slot
+export const createSlot = async (req, res) => {
+  try {
+    const newSlot = await Slot.create(req.body);
+    res.status(201).json(newSlot);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Function used to get all slots
+export const getAllSlots = async (req, res) => {
+  try {
+    const slots = await Slot.find();
+    res.status(200).json(slots);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Function used to update slot by ID
+export const updateSlot = async (req, res) => {
+  try {
+    const updatedSlot = await Slot.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedSlot);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Function used to delete slot by ID
+export const deleteSlot = async (req, res) => {
+  try {
+    await Slot.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
---
+++ controllers/UserController.js
@@ -0,0 +1,41 @@
+import User from "../models/users.js";
+
+// Function used to create new user
+export const createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Function used to get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Function used to update user by ID
+export const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Function used to delete user by ID
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
