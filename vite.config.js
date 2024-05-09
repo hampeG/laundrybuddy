d36fe27b1345202a38b57355332f20a9b0e5a5d2
Hampus Gunnarsson <hampus.gunnarsson@hotmail.com>
2024-05-09 19:00:39 +0200

    KAN-13 Change vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
     proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
-})
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+});
