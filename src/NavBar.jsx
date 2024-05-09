fba78f967ea97b607025edfeac6ac879728423eb
Zgjim Zhubaj <zgjimzhubaj1@gmail.com>
2024-05-09 16:38:27 +0200

    KAN-26 Made some changes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faAddressBook, faCalendarAlt , faInfoCircle, faUser,faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
+import logo from './lb-logo.png'; 
 
 const NavBar = () => {
   return (
     <Navbar expand="lg" fixed="top" className="custom-navbar">
-      <Navbar.Brand href="#home" className="navbar-brand" style={{ position: 'absolute', right: '1080px', top: '20px' }}>
+      <Navbar.Brand href="#">
         <img
-          src="src/lb-logo.png"
-          alt="logo"        
-          style={{ width: '150px', height: '53px' }}
+          src={logo}
+          alt="logo"
+          style={{ width: '160px', height: '53px' }}
         />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
         <Nav className="navbar-nav">
-          <Nav.Link href="#"style={{ color: 'white', marginRight: '20px', fontSize: '20px' }}>Main</Nav.Link>
-          <Nav.Link href="#"style={{ color: 'white', marginRight: '20px', fontSize: '20px' }}>Booking</Nav.Link>
-          <Nav.Link href="#"style={{ color: 'white', marginRight: '20px', fontSize: '20px' }}>About</Nav.Link>
-          <Nav.Link href="#"style={{ color: 'white', marginRight: '20px', fontSize: '20px' }}>Contact</Nav.Link>
+          <Nav.Link href="#" className="nav-link"> <FontAwesomeIcon icon={faHome} />   Main</Nav.Link>
+          <Nav.Link href="#" className="nav-link"><FontAwesomeIcon icon={faCalendarAlt} />   Booking</Nav.Link>
+          <Nav.Link href="#" className="nav-link"><FontAwesomeIcon icon={faInfoCircle} />   About</Nav.Link>
+          <Nav.Link href="#" className="nav-link"><FontAwesomeIcon icon={faAddressBook} />   Contact</Nav.Link>
         </Nav>
-        <Nav className="ml-auto" style={{ position: 'absolute', left: '940px', top: '22px' }}>
-          <Button variant="light" style={{ width: '278px', height: '53px', borderRadius: '30px', backgroundColor: '#FFD02C', color: 'white', fontSize: '30px', fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Login/Register</Button>
+        <Nav className="ml-auto justify-content-end">
+            <Button variant="light" className="navbar-btn"><FontAwesomeIcon icon={faUser} /> Sign Up</Button>
+            <Button variant="light" className="navbar-btn"><FontAwesomeIcon icon={faSignInAlt} /> Login</Button>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
