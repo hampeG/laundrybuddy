bc2db28bb3b256b51c9e319834ee28f9a8eadd4f
Hampus Gunnarsson <hampus.gunnarsson@hotmail.com>
2024-05-14 00:53:28 +0200

    KAN-28 Implement logout functionality from NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faAddressBook, faCalendarAlt , faInfoCircle, faUser,faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faAddressBook, faCalendarAlt , faInfoCircle, faUser, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext'; 
 
 const NavBar = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth(); 
+  const navigate = useNavigate();
+
+  // Function to handle logout
+  const handleLogout = () => {
+    logout();  
+    navigate('/login');  // Redirect to the login page after logout
+  };
 
   return (
     <Navbar expand="lg" fixed="top" className="custom-navbar">
@@ -21,7 +28,7 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
         <Nav className="navbar-nav">
-          <Nav.Link className="nav-link"><Link to="/" className="link"><FontAwesomeIcon icon={faHome} />   Main</Link></Nav.Link>
+          <Nav.Link href="#" className="nav-link"><Link to="/" className="link"><FontAwesomeIcon icon={faHome} />   Main</Link></Nav.Link>
           <Nav.Link href="#" className="nav-link">
             {user ? (
               <Link to="/book" className="link"><FontAwesomeIcon icon={faCalendarAlt} />   Booking</Link>
@@ -33,7 +40,11 @@ const NavBar = () => {
           <Nav.Link href="#" className="nav-link"><Link to="/contact" className="link"><FontAwesomeIcon icon={faAddressBook} />   Contact</Link></Nav.Link>
         </Nav>
         <Nav className="ml-auto justify-content-end">
+          {user ? (
+            <Button variant="light" className="navbar-btn" onClick={handleLogout}><FontAwesomeIcon icon={faSignInAlt} /> Logout</Button>
+          ) : (
             <Button variant="light" className="navbar-btn"><FontAwesomeIcon icon={faSignInAlt} /><Link to="/login" className="link">Login/Register</Link></Button>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
