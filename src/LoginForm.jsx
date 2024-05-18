0c093e872de48c04b5bfc20622ece737576ab347
Zgjim Zhubaj <zgjimzhubaj1@gmail.com>
2024-05-18 17:06:26 +0200

    Styled login form and made it responsive

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useAuth } from "./context/AuthContext";
 import { Link } from "react-router-dom";
+import './ContactForm.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 function LoginForm() {
   const [formData, setFormData] = useState({
@@ -19,6 +22,9 @@ function LoginForm() {
       [name]: value
     }));
   }
+  const handleRatingChange = ratingValue => {
+    setFormData({ ...formData, rating: ratingValue });
+  };
 
   // Event handler for form submission
   async function handleSubmit(e) {
@@ -34,7 +40,18 @@ function LoginForm() {
 
   return (
     <div>
-      <h2>LaundryBuddy logo goes here again</h2>
+      <div class="contact-form-container">
+        <p className="contact-form-text">
+        <FontAwesomeIcon icon={faSignInAlt} className="contact-icon" />
+          Login Form
+        </p>
+      </div>
+    <div className='body1'>
+    <img 
+                src="src/images/lb-logo-sq 1.png"
+                alt="Laundry Buddy Logo"
+                className="Logo"
+            />
       {errorMessage && <div className="error">{errorMessage}</div>}
       <form onSubmit={handleSubmit}>
         <div>
@@ -44,8 +61,25 @@ function LoginForm() {
           <input type="password" placeholder="Password" id="loginPassword" name="password" autoComplete="off" value={formData.password} onChange={handleChange} required />
         </div>
         <button type="submit">Login</button>
-        <p><Link to="/register">Not a member? Create account here</Link></p>
+        <p className="register-link"><Link to="/register">Not a member? Create account here</Link></p>
       </form>
+    </div>
+    <div className="rating-container">
+        <p className='rating-text'>Rate your experience:</p>
+        <div className="stars">
+            <input type="radio" id="star1" name="rating" value="1" checked={formData.rating === '1'} onChange={() => handleRatingChange('1')} />
+            <label htmlFor="star1">&#9733;</label>
+            <input type="radio" id="star2" name="rating" value="2" checked={formData.rating === '2'} onChange={() => handleRatingChange('2')} />
+            <label htmlFor="star2">&#9733;</label>
+            <input type="radio" id="star3" name="rating" value="3" checked={formData.rating === '3'} onChange={() => handleRatingChange('3')} />
+            <label htmlFor="star3">&#9733;</label>
+            <input type="radio" id="star4" name="rating" value="4" checked={formData.rating === '4'} onChange={() => handleRatingChange('4')} />
+            <label htmlFor="star4">&#9733;</label>
+            <input type="radio" id="star5" name="rating" value="5" checked={formData.rating === '5'} onChange={() => handleRatingChange('5')} />
+            <label htmlFor="star5">&#9733;</label>
+        </div>
+
+      </div>
     </div>
   );
 }
