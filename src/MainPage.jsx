2fd748405db7db682896239813a5439f4eb6219b
Sam <samsse46@gmail.com>
2024-05-20 17:18:25 +0200

    preparing for deployment

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,37 +1,39 @@
-import { Container, Button } from 'react-bootstrap';
-import './MainPage.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from './context/AuthContext';
+import { Container, Button } from "react-bootstrap";
+import "./MainPage.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 
 const MainPage = () => {
-    const { user } = useAuth(); // Getting the user from AuthContext
-    const navigate = useNavigate();
+  const { user } = useAuth(); // Getting the user from AuthContext
+  const navigate = useNavigate();
 
-    const handleBookNowClick = () => {
-        const redirectPath = '/book';
-        navigate('/login', { state: { redirectPath } });
-    };
+  const handleBookNowClick = () => {
+    const redirectPath = "/book";
+    navigate("/login", { state: { redirectPath } });
+  };
 
-    return (
-        <Container fluid className="mainContainer">
-            <div className="content">
-                <h2>Book Your Laundry Time</h2>
-                <h1>Right On Your Device.</h1>
-                {user ? (
-                    <Link to="/book" className="link">
-                        <Button className="bookButton">BOOK NOW</Button>
-                    </Link>
-                ) : (
-                    <Button className="bookButton link" onClick={handleBookNowClick}>BOOK NOW</Button>
-                )}
-            </div>
-            <img 
-                src="src/images/laundry-hero.png"
-                alt="Laundry Buddy"
-                className="heroImage"
-            />
-        </Container>
-    );
+  return (
+    <Container fluid className="mainContainer">
+      <div className="content">
+        <h2>Book Your Laundry Time</h2>
+        <h1>Right On Your Device!</h1>
+        {user ? (
+          <Link to="/book" className="link">
+            <Button className="bookButton">BOOK NOW</Button>
+          </Link>
+        ) : (
+          <Button className="bookButton link" onClick={handleBookNowClick}>
+            BOOK NOW
+          </Button>
+        )}
+      </div>
+      <img
+        src="src/images/laundry-hero.png"
+        alt="Laundry Buddy"
+        className="heroImage"
+      />
+    </Container>
+  );
 };
 
 export default MainPage;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Determine if we are in the development environment
+const isDevelopment = process.env.NODE_ENV === "development";
+
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
+    proxy: isDevelopment
+      ? {
+          "/api": {
+            target: "http://localhost:3000",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, "/api"),
+          },
+        }
+      : undefined,
   },
 });
